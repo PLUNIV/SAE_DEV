c31dc7958a3cea95464b78b9099256ed9d923d95
debuyssp <pierre-louis.debuysschere@etu.univ-smb.fr>
2022-01-10 10:39:00 +0100

    test 2

--- SAE/SAE/Game1.cs
+++ SAE/SAE/Game1.cs
@@ -42,7 +42,7 @@ namespace SAE
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Red);
+            GraphicsDevice.Clear(Color.Green);
 
             // TODO: Add your drawing code here
 
