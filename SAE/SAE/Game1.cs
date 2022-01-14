6bce8e7021eeaae1c89683d2ad18c78684c5bf96
Noah Lamy <Noah.Lamy@etu.univ-smb.fr>
2022-01-14 18:26:24 +0100

    Ajout fantome

--- SAE/SAE/Game1.cs
+++ SAE/SAE/Game1.cs
@@ -9,6 +9,8 @@ using MonoGame.Extended.Animations;
 using Microsoft.Xna.Framework.Content;
 
 using System;
+using MonoGame.Extended.TextureAtlases;
+using System.Collections.Generic;
 
 namespace SAE
 {
@@ -19,11 +21,19 @@ namespace SAE
         private SpriteBatch _spriteBatch;
         private Vector2 _persoPosition;
         private int _vitessePerso;
+        private Vector2 _positionFantome;
         private AnimatedSprite _perso;
+        private AnimatedSprite _FantomeGros;
+        private AnimatedSprite _FantomeBase;
+        private AnimatedSprite _FantomePetit;
         private Song song;
-        private int _VitesseFantomeRouge;
-        private int _VitesseFantomeBleu;
-        private int _VitesseFantomeJaune;
+        private int _VitesseFantomePetit;
+        private int _VitesseFantomeBase;
+        private int _VitesseFantomeGros;
+
+        private AnimatedSprite[] monsters;
+        private Vector2[] monsterPositions;
+      
 
         public Game1()
         {
@@ -37,9 +47,10 @@ namespace SAE
             // TODO: Add your initialization logic here
             _persoPosition = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height - 100);
             _vitessePerso = 100;
-            _VitesseFantomeJaune = 100;
-            _VitesseFantomeRouge = 85;
-            _VitesseFantomeBleu = 70;
+            _VitesseFantomePetit = 100;
+            _VitesseFantomeBase = 85;
+            _VitesseFantomeGros = 70;
+
             base.Initialize();
         }
 
@@ -49,6 +60,18 @@ namespace SAE
             // spritesheet
             SpriteSheet spriteSheet = Content.Load<SpriteSheet>("Sprites.sf", new JsonContentLoader());
             _perso = new AnimatedSprite(spriteSheet);
+            _FantomePetit = new AnimatedSprite(spriteSheet, "fantôme petit");
+            
+            monsters = new AnimatedSprite[3];
+            monsters[0] = new AnimatedSprite(spriteSheet, "fantôme petit");
+            monsters[1] = new AnimatedSprite(spriteSheet, "fantôme base");
+            monsters[2] = new AnimatedSprite(spriteSheet, "fantôme gros");
+
+            monsterPositions = new Vector2[3];
+            monsterPositions[0] = new Vector2(10, 10);
+            monsterPositions[1] = new Vector2(850, 10);
+            monsterPositions[2] = new Vector2(10, 400);
+
             this.song = Content.Load<Song>("hauntedcastle");
             MediaPlayer.Play(song);
             MediaPlayer.IsRepeating = true;
@@ -70,6 +93,11 @@ namespace SAE
             float walkSpeed = deltaSeconds * _vitessePerso;
             
             _perso.Update(deltaSeconds);
+            _FantomePetit.Update(deltaSeconds);
+            foreach (AnimatedSprite monster in monsters)
+            {
+                monster.Update(deltaSeconds);
+            }
             // TODO: Add your update logic here
             KeyboardState keyboardState = Keyboard.GetState();
             string animation = "idle droite";
@@ -92,8 +120,24 @@ namespace SAE
                 _persoPosition.X += walkSpeed;
             }
 
+
+            for (int i = 0; i < monsters.Length; i++)//pour tout les mobs
+            {
+                //monsters[i] le mob
+                //monsterPositions[i] la position du mob
+
+                //faire l'animation du mob (son déplacement)
+            }
+
             _perso.Play(animation);
             _perso.Update(deltaSeconds);
+            _FantomePetit.Play("fantôme petit");
+            _FantomePetit.Update(deltaSeconds);
+
+            foreach (AnimatedSprite monster in monsters)
+            {
+                monster.Update(deltaSeconds);
+            }
 
             base.Update(gameTime);
         }
@@ -105,7 +149,11 @@ namespace SAE
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
             _spriteBatch.Draw(_perso, _persoPosition);
-
+            _spriteBatch.Draw(_FantomePetit, _persoPosition);
+            for (int i = 0; i < monsters.Length; i++)
+            {
+                _spriteBatch.Draw(monsters[i], monsterPositions[i]);
+            }
             _spriteBatch.End();
             base.Draw(gameTime);
         }
